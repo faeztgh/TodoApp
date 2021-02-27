d38e214952d4251b9ea9f3cabdfa4beb539936f9
faez <faeztgh@users.noreply.github.com>
2021-02-27 17:00:37 +0330

    adding Todos component to App component and handle functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Todos from "./components/routes/Todos";
+import { Data } from "./data/data";
 
 function App() {
-    return <></>;
+    const [todos, setTodos] = useState<Todo[]>(Data);
+    const [unDoneTodos, setUnDoneTodos] = useState<Todo[]>([]);
+
+    useEffect(() => {
+        setUnDoneTodos(todos.filter((todo) => !todo.isDone));
+    }, [todos]);
+
+    const handleStatusChange: HandleStatusChange = (id) => {
+        const newTodos = todos.map((todo) => {
+            if (todo.id === id) {
+                return {
+                    ...todo,
+                    isPaused: !todo.isPaused,
+                };
+            }
+            return todo;
+        });
+        setTodos(newTodos);
+    };
+
+    const handleIsDone: HandleIsDone = (id) => {
+        const newTodos = todos.map((todo) => {
+            if (todo.id === id) {
+                return {
+                    ...todo,
+                    isDone: !todo.isDone,
+                };
+            }
+            return todo;
+        });
+
+        setTodos(newTodos);
+    };
+
+    return (
+        <>
+            <div className="m-16 lg:m-32 md:m-20">
+                <Todos
+                    todos={unDoneTodos}
+                    handleStatusChange={handleStatusChange}
+                    handleIsDone={handleIsDone}
+                />
+            </div>
+        </>
+    );
 }
 
 export default App;
